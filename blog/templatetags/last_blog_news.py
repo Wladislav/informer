9ee9027ac1c94153c61ace33002ab1b1773a8f64
Wladislav <photocritic72@gmail.com>
2017-03-14 18:17:39 +0300

    faq added

--- blog/templatetags/last_blog_news.py
+++ blog/templatetags/last_blog_news.py
@@ -1,13 +1,16 @@
 from django import template
 from wagtail.wagtailcore.models import Page
+from blog.models import BlogPage
 from django.template.defaultfilters import truncatechars_html
+from django.contrib.contenttypes.models import ContentType
 import re
 
 register = template.Library()
 
 @register.inclusion_tag('blog/blog_page_widget.html')
 def last_blog_news(value):
-    some_pagr_qs = Page.objects.filter(live=1, content_type_id=37, expired=0).order_by('latest_revision_created_at')[:int(value)]
+    content_type_id =  ContentType.objects.get_for_model(BlogPage).pk #37
+    some_pagr_qs = Page.objects.filter(live=1, content_type_id=content_type_id, expired=0).order_by('latest_revision_created_at')[:int(value)]
     for page_res in some_pagr_qs:
         # Obrezaem do 130 simvolov
         truncated_text = truncatechars_html(page_res.specific.body.strip('\r'),150)
